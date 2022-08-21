7cdbc4573ddcf484ce666d2b1ed0fa9aa4c1668d
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-08-21 16:51:36 +0300

    i hate mcdev

--- src/main/java/adudecalledleo/graytomorrow/mixin/client/StuckObjectsFeatureRendererMixin.java
+++ src/main/java/adudecalledleo/graytomorrow/mixin/client/StuckObjectsFeatureRendererMixin.java
@@ -9,14 +9,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.StuckObjectsFeatureRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
 @Mixin(StuckObjectsFeatureRenderer.class)
 public abstract class StuckObjectsFeatureRendererMixin {
-	@Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/Entity;FFFFFF)V",
+	@Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V",
 			at = @At("HEAD"), cancellable = true)
 	private void graytomorrow$skipRenderingIfFullyInvisible(MatrixStack matrices,
-			VertexConsumerProvider vertexConsumers, int light, Entity entity, float limbAngle, float limbDistance,
+			VertexConsumerProvider vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance,
 			float tickDelta, float animationProgress, float headYaw, float headPitch, CallbackInfo ci) {
 		if (entity.isInvisible() && GrayTomorrowComponents.get(GrayTomorrowComponents.EQUIPMENT_INVISIBLE, entity)) {
 			ci.cancel();
