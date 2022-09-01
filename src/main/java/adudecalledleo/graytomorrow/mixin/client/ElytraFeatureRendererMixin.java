1e753a59bfd00dbaf019f78bd0d6c2555e33d61c
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-09-01 17:19:56 +0300

    Fix Elytras not being invisible when "equipment invisible" flag is set

---
+++ src/main/java/adudecalledleo/graytomorrow/mixin/client/ElytraFeatureRendererMixin.java
@@ -0,0 +1,26 @@
+package adudecalledleo.graytomorrow.mixin.client;
+
+import adudecalledleo.graytomorrow.GrayTomorrowComponents;
+import adudecalledleo.graytomorrow.util.BooleanComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+
+@Mixin(ElytraFeatureRenderer.class)
+public abstract class ElytraFeatureRendererMixin {
+	@Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V",
+			at = @At("HEAD"), cancellable = true)
+	private void graytomorrow$skipRenderingIfEquipmentInvisible(MatrixStack matrices,
+			VertexConsumerProvider vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance,
+			float tickDelta, float animationProgress, float headYaw, float headPitch, CallbackInfo ci) {
+		if (BooleanComponent.get(GrayTomorrowComponents.EQUIPMENT_INVISIBLE, entity)) {
+			ci.cancel();
+		}
+	}
+}
