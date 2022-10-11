d6d1ce40bdd8f487b61fa3cb25ba5c5b9028bdd7
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-10-11 12:10:23 +0300

    Ears compat: eliminate clipping features
    Inhibit all torso-anchored features when cloak is equipped, except for tails (previously only wings and capes were inhibited)
    Inhibit all head-anchored features when hood is up, except for snouts (previously only ears were inhibited)

--- src/main/java/adudecalledleo/graytomorrow/compat/client/GrayTomorrowEarsCompat.java
+++ src/main/java/adudecalledleo/graytomorrow/compat/client/GrayTomorrowEarsCompat.java
@@ -2,6 +2,7 @@ package adudecalledleo.graytomorrow.compat.client;
 
 import adudecalledleo.graytomorrow.CloakItem;
 import adudecalledleo.graytomorrow.GrayTomorrow;
+import com.unascribed.ears.api.EarsAnchorPart;
 import com.unascribed.ears.api.EarsFeatureType;
 import com.unascribed.ears.api.registry.EarsInhibitorRegistry;
 import net.fabricmc.api.EnvType;
@@ -24,12 +25,12 @@ public final class GrayTomorrowEarsCompat {
 			case NOT_EQUIPPED:
 				return false;
 			case HOOD_UP:
-				if (type == EarsFeatureType.EARS) {
+				if (type != EarsFeatureType.SNOUT && type.isAnchoredTo(EarsAnchorPart.HEAD)) {
 					return true;
 				}
 				// fallthrough
 			case HOOD_DOWN:
-				return type == EarsFeatureType.WINGS || type == EarsFeatureType.CAPE;
+				return type != EarsFeatureType.TAIL && type.isAnchoredTo(EarsAnchorPart.TORSO);
 			default:
 				throw new IllegalStateException("unhandled CloakState " + state);
 		}
